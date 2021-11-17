0c18ef4ff1633e0b6fab9270bb3620fdb91247a1
Mihailob5017 <mihailob5017@gmail>
2021-11-17 18:29:21 +0100

    set up psql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello  world');
+import { MikroORM } from '@mikro-orm/core';
+
+const main = async () => {
+	console.log('hello  woyatnrld');
+	const orm = await MikroORM.init({
+		dbName: 'lireddit',
+		user: 'postgres',
+		password: 'jsmv4183',
+		debug: process.env.NODE_ENV !== 'production',
+		type: 'postgresql',
+	});
+};
+
+main();
